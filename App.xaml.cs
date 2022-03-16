93ad156993a1d9af6034d36424358112f57c6c3f
OCHIAI Tsuyoshi <ochiai@kems.group>
2022-03-16 09:17:59 +0900

    Support #6

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,17 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace FilePairing
 {
-	/// <summary>
-	/// App.xaml の相互作用ロジック
-	/// </summary>
-	public partial class App : Application
-	{
-	}
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App
+    {
+        // Logger
+        private static readonly log4net.ILog Logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod()?.DeclaringType);
+
+
+        // Main method
+        [STAThreadAttribute()]
+        public static void Main()
+        {
+            var app = new App();
+            app.InitializeComponent();
+            app.Startup += App_Startup;
+
+            AppDomain.CurrentDomain.UnhandledException += (sender, e) =>
+            {
+                if (!(e.ExceptionObject is Exception exception)) return;
+
+                Logger.Error(exception);
+                MessageBox.Show("アプリケーションで例外が発生しました。内容をログファイルに記述し、アプリケーションを終了します。", "システムエラー", MessageBoxButton.OK, MessageBoxImage.Error);
+            };
+
+            app.Run();
+        }
+
+
+        /// <summary>
+        /// Startup event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void App_Startup(object sender, StartupEventArgs e)
+        {
+            new MainWindow().Show();
+        }
+    }
 }
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -53,3 +53,7 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+
+// Logger
+[assembly: log4net.Config.XmlConfigurator(ConfigFile = "./log4net.xml", Watch = false)]
